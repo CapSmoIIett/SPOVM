2944cdd5966a25794705180dcd88eb07c9382772
Matthew <vikdrovosek@gmail.com>
2021-03-22 15:20:42 +0300

    Теперь оно, вроде, читает.

--- Lab_5/mylib.c
+++ Lab_5/mylib.c
@@ -10,7 +10,7 @@ struct aiocb createIoRequest(int fd,
                             volatile void * content, 
                             size_t length){
     // создает и инициализирует aiocb структуру
-    // Если мы не инициализируем нулем, то у нас бужет неопределенный результат
+    // Если мы не инициализируем нулем, то у нас может быть неопределенный результат
 
     // E.g. through sigevent op.aio_sigevent there could be 
     //      a callback function being set, that the program
@@ -30,9 +30,36 @@ int fileWrite();
 
 int fileRead()
 {
-    printf("Read\n");
-    return 100;
+    FILE* file = fopen("outfile.txt","r");
+    int fd = fileno(file);                                      // Файловый дескриптор
+    char buf[20];
+    {
+        
+        struct aiocb op  = createIoRequest(fd,0, buf, 20);        
+
+        //aio_write(&op);
+        int ret = aio_read(&op);
+        printf("aio_write 1: %d\n",ret);
+
+        {
+            const struct aiocb * aiolist[1];
+            aiolist[0] = &op;
+
+            int ret = aio_suspend(aiolist,1,NULL);
+            printf("aio_suspend: %d\n",ret);
+
+            // report possible errors
+            {
+                ret = aio_error(&op);
+                printf("errno 1: %d\n",ret);
+            }
+        }
+    }
+    printf("%s\n",buf);
+    fclose(file);
+    return 0;
 }
+
 int fileWrite()
 {
     FILE* file = fopen("outfile.txt","w");
@@ -59,7 +86,5 @@ int fileWrite()
         }
     }
     fclose(file);
-
-    printf("Write\n");
     return 0;
 }
