cf37d75965ec2b504be191a9fae21086d16c5c8c
Matthew <vikdrovosek@gmail.com>
2021-04-14 10:18:35 +0300

    Дописал Readme

--- Lab_5/main.c
+++ Lab_5/main.c
@@ -76,7 +76,6 @@ int main (){
     usleep(100000);
     //ret = pthread_join(thReader, NULL);
     ret = pthread_join(thWrite, NULL);                             // Ждем окончания потока записи. Он закончится позже потока чтения 
-    // НУЖНО ЖДАТЬ ПОКА ПОТОКИ ЗАКРОЮТСЯ
 
     closeThread (thReader);                                         // Закрываем потоки
     closeThread (thWrite);
